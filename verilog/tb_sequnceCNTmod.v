c3b81e42aada06d4fec616f131f4fdf190570b37
iulia <iulia.vintan@student.upt.ro>
2025-04-15 23:23:36 +0300

    incercare control unit

--- verilog/tb_sequnceCNTmod.v
+++ verilog/tb_sequnceCNTmod.v
@@ -1,5 +1,4 @@
-// Code your testbench here
-// or browse Examples
+
 `timescale 1ns / 1ps
 
 module tb_sequnceCNTmod;
@@ -32,57 +31,34 @@ module tb_sequnceCNTmod;
 
   // Inițializare și aplicare stimuli
   initial begin
+     $dumpfile("dump.vcd"); $dumpvars;
+    CLK=0;
+  end
+  
+  always begin
+     $dumpfile("dump.vcd"); $dumpvars;
+    #10 CLK=~CLK;
+  end
+  
+  initial begin
     $dumpfile("dump.vcd"); $dumpvars;
     $display("Time\tCLK\tBGN\tRST\tEND\tfi0\tfi1\tfi2\tfi3\tfi4\tfi5");
     $monitor("%g\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b", $time, CLK, BGN, RST, END, fi0, fi1, fi2, fi3, fi4, fi5);
 
     // Inițializare
-    BGN=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-  	END=0;
-    CLK=0; #10;
+    BGN=1; 
+  	END=0; 
     RST=0; 
-    CLK=1; #10;
+    #10;
     RST=1;
-    CLK=0; #10;
+    #10;
     RST=0;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    END=1;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
-    CLK=0; #10;
-    CLK=1; #10;
+   	#210;
+   	RST=1;
+    #10;
+    RST=0;
+    #100;
+
 
 
     $finish;
